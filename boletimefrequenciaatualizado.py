52044233a53103b2ee9a626561ea7d0e3c28a98f
Davidev0 <162806102+Davidev0@users.noreply.github.com>
2026-05-30 10:26:27 -0300

    Add files via upload

---
+++ boletimefrequenciaatualizado.py
@@ -0,0 +1,28 @@
+print("===== BOLETIM RSCOLAR ======")
+n1=float(input("Nota 1 :"))
+n2=float(input("Nota 2 :"))
+n3=float(input("Nota 3 :"))
+n4=float(input("Nota 4 : "))
+n5=float(input("Nota 5 :"))
+n6=float(input("Nota 6 : "))
+n7=float(input("Nota 7 :"))
+n8=float(input("Nota 8 :"))
+media=(n1+n2+n3+n4+n5+n6+n7+n8)/8
+print(f"\nMédia: {media:.1f}")
+if media == 10 : 
+    print("Perfeito Nota maxima ")
+elif media>=9:
+    print("Excelente Desempenho")
+elif media>=7:
+    print("Aprovado !")
+elif media>= 5:
+    print("Recuperação")
+elif media>= 0:
+    print("Reprovado")
+frequencia = float(input("Frequência (%): "))
+print(f"Frequência: {frequencia}%")
+if frequencia<75:
+    print("Reprovado por falta")
+else:
+    print("Fim de Bimestre ")
+
