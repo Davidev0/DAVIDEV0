1571403aee881d91391b61442ff2af6bd43bf964
Davidev0 <162806102+Davidev0@users.noreply.github.com>
2026-06-09 17:10:30 -0300

    Add files via upload

---
+++ calendariobasico.py
@@ -0,0 +1,5 @@
+import calendar
+ano=int(input("Digite o ano :"))
+mes=int(input("Digite o mês(1-12):"))
+print("\n")
+print(calendar.month(ano, mes))
---
+++ velocidademedia.py
@@ -0,0 +1,4 @@
+distancia=float(input("Digite a distância percorrida (m) :"))
+tempo=float(input("Digite o tempo gasto (s) :"))
+velocidade=distancia/tempo
+print(f"Velocidade média : {velocidade :.2f} m/s")
