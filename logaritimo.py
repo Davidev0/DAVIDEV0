a809f1ebc48fb82c3fad5df87a8554e2c5b1ade4
Davidev0 <162806102+Davidev0@users.noreply.github.com>
2026-06-23 22:44:47 -0300

    Add files via upload

---
+++ logaritimo.py
@@ -0,0 +1,5 @@
+import math
+numero=int(input("Digite o numero :"))
+base=float(input("Digite a Base do logaritimo :"))
+resultado=math.log(numero, base)
+print(f"Log na base {base} de  {numero}={resultado}")
