35fc1501543090345ca926ecd3bcf3be0b49dc77
Davidev0 <162806102+Davidev0@users.noreply.github.com>
2026-07-04 21:39:26 -0300

    Add files via upload

---
+++ regrade3.py
@@ -0,0 +1,7 @@
+print("===== REGRA DE 3 ====")
+a=float(input("Digite o primeiro valor :"))
+b=float(input("Digite o segundo valor : "))
+c=float(input("Digite o terceiro valor : "))
+x=(b * c)/a
+print(f"\nO Valor de x é:{x:.2f}")
+
